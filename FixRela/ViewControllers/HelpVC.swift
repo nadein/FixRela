4dcc5d00871cd1720f751b54e49b338c1a65eee2
Alex Nadein <>
2018-12-07 22:16:49 +0200

    Add basic tab bar application project.

---
+++ FixRela/ViewControllers/HelpVC.swift
@@ -0,0 +1,19 @@
+//
+//  FirstViewController.swift
+//  FixRela
+//
+//  Created by Alex Nadein on 12/7/18.
+//  Copyright © 2018 Alex Nadein. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
---
+++ FixRela/ViewControllers/SocialVC.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  FixRela
+//
+//  Created by Alex Nadein on 12/7/18.
+//  Copyright © 2018 Alex Nadein. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
